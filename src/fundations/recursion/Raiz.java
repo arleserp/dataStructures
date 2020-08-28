61110af15d642b0af69f76149925f1357ed005c1
arleserp <arles.rodriguez@gmail.com>
2020-08-28 15:33:46 -0500

    Adición de la raiz cuadrada de un número real.

---
+++ src/fundations/recursion/Raiz.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundations.recursion;
+
+/**
+ *
+ * @author Arles
+ */
+public class Raiz {
+
+    private static double buscar(double n, double st, double end) {
+        double mid = (st + end) / 2;
+        if (Math.abs(mid * mid - n) < 1e-12) {
+            return mid;
+        } else if (mid * mid > n) {
+            return buscar(n, st, mid);
+        } else {
+            return buscar(n, mid, end);
+        }
+    }
+
+    public static double raiz(double n) {
+        return buscar(n, 0, n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(raiz(2.0));
+    }
+}
+
