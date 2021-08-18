7cd9a7792486cbff67a4be5f8fa7a933152a887c
arleserp <arles.rodriguez@gmail.com>
2021-08-18 14:06:51 -0500

    Addition of Default interfaces and constants in interfaces.

---
+++ src/foundations/java/implementa/classicTeleport.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foundations.java.implementa;
+
+/**
+ *
+ * @author Arles
+ */
+public class classicTeleport implements DefaultTeleport {
+    public static void main(String[] args) {
+        classicTeleport c = new classicTeleport();
+        c.teleport("jupyter");
+    }
+}
